0fc377f8d5368eac59030fe752ae9a916f2d3816
WeaponXIDev <mondalabhijit599@gmail.com>
2024-09-19 12:55:41 +0000

    dataTypes

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 144553  //cant change value
+let accountEmail = "demo@gmail.com"
+var accountPassword = "12345"
+accountCity = "Kolkata"
+let accountState
+
+
+// accountId = 2
+accountEmail = "demo2@gmail.com"
+accountPassword = "212121"
+accountCity = "Delhi"
+console.log(accountId)
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,35 @@
+"use strict"; //treat all js code as newer version 
+
+//alert('hello') WE ARE USING NODE JS NOT BROWSER 
+
+console.log(3 +
+    3
+) //code redability should be high 
+
+console.log('data')
+
+
+let name = "abhi" //string
+let age = 18 //number
+let isLoggedIn = true //boolean
+let state = null
+let city
+
+/*
+premetive data types
+//number => 2 to power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone value  / represent of empty value
+//undefined => no value defined 
+//symbol => unique
+*/
+
+/*
+object
+*/ 
+
+
+console.log(typeof null) //object
+console.log(typeof undefined) //undefined
